c3f1c7b72ba1a70be37f2f6bdaa93de756769ee5
michaldolnak <mdolnak@kkmbfin.sk>
2020-06-21 13:46:57 +0200

    Add 5 desired functions, test functionality, switch to nodemon

--- imageresizer/libs/imageresizer/imageresizer.js
+++ imageresizer/libs/imageresizer/imageresizer.js
@@ -1,28 +1,46 @@
 const sharp = require('sharp');
+const fsPromises = require('fs').promises;
 
 const middleware = {
-	fill: async function(s, width, height) {
-		return s.resize(width, height, {fit:'fill'});
-	}
+	contain: async function(s, width, height) {
+		return s.resize(width, height, {fit:'contain'});
+	},
+	autoScaleHeight: async function(s, width) {
+		return s.resize(width, null, {fit:'contain'});
+	},
+	autoScaleWidth: async function(s, height) {
+		return s.resize(null, height, {fit:'contain'});
+	},
+	outside: async function(s, width, height) {
+		return s.resize(width, height, {fit:'outside'});
+	},
+	cover: async function(s, width, height) {
+		return s.resize(width, height, {fit:'cover'});
+	},
 };
 
 const imageresizer = {
 	resize: async function(path, width, height, method, format) {
 		let s = sharp(path);
-		switch (format) {
+		switch (method) {
 			case 'Fit':
+			await middleware.contain(s, width, height);
 			break;
 			case 'ScaleWidth':
+			await middleware.autoScaleHeight(s, width);
 			break;
 			case 'ScaleHeight':
+			await middleware.autoScaleWidth(s, height);
 			break;
 			case 'Fill':
-			let res = await sharp.fill(s, width, height);
+			await middleware.outside(s, width, height);
 			break;
-
 			case 'Crop':
+			await middleware.cover(s, width, height);
 			break;
 		}
+		await s.toFile('output.png');
+		await fsPromises.rename('output.png', path);
 	}
 };
 
--- imageresizer/routes/index.js
+++ imageresizer/routes/index.js
@@ -5,10 +5,16 @@ const imageresizer = require('../libs/imageresizer/imageresizer');
 /* GET home page. */
 router.get('/', async(req, res) => {
 
+  params = {};
   params.body = {
+      path:'/Users/michaldolnak/VSETCI_MOZU_CITAT/imageResizer/imageresizer/chart.png',
+	  width:100,
+      height:100,
+      method:'ScaleHeight',
+      format: 'png'
+  };
 
-  }
-  res = await imageresizer.resize(params.body.path, params.body.width, params.body.height, params.body.method, params.body.format);
+  await imageresizer.resize(params.body.path, params.body.width, params.body.height, params.body.method, params.body.format);
   res.render('index', { title: 'Express' });
 });
 
