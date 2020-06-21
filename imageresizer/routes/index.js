cca2a91a7228fe351acf8bdd050b50b2aab1fa14
michaldolnak <mdolnak@kkmbfin.sk>
2020-06-21 18:21:16 +0200

    Add web image-resize form, connect it api call, add bootstrap

--- imageresizer/routes/index.js
+++ imageresizer/routes/index.js
@@ -4,18 +4,13 @@ const imageresizer = require('../libs/imageresizer/imageresizer');
 
 /* GET home page. */
 router.get('/', async(req, res) => {
-
-  params = {};
-  params.body = {
-      path:'/Users/michaldolnak/VSETCI_MOZU_CITAT/imageResizer/imageresizer/chart.png',
-	  width:100,
-      height:100,
-      method:'ScaleHeight',
-      format: 'png'
-  };
-
-  await imageresizer.resize(params.body.path, params.body.width, params.body.height, params.body.method, params.body.format);
   res.render('index', { title: 'Express' });
 });
+
+router.post('/image-resize', async(req, res) => {
+	await imageresizer.resize(req.body.path, parseInt(req.body.width), parseInt(req.body.height), req.body.method, req.body.format);
+	res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;
